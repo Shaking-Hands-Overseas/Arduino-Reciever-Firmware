bd2ed73dd52ac2339313ad68a742b8ce140dfeb4
Gondoraragorn <Joel.garcia1202@hotmail.com>
2022-04-11 20:56:07 +0200

    Added Button reading

--- Reciever/Reciever.ino
+++ Reciever/Reciever.ino
@@ -2,7 +2,13 @@
 
 Servo servo1, servo2, servo3,servo4, servo5;
 
+#define But1 8
+#define But2 9
+
+
 void setup() {
+  pinMode(But1, INPUT);
+  pinMode(But2, INPUT);
   servo1.attach(2);
   servo2.attach(3);
   servo3.attach(4);
@@ -13,6 +19,8 @@ void setup() {
 }
 
 void loop() {
+  int b1 = digitalRead(But1);
+  int b2 = digitalRead(But1);
   while (!Serial.available());
   delay(100); //Adding some delay so that the serial port is not overflowed
   String x1 = Serial.readStringUntil('\n'); //Reading a hole string of data
@@ -21,7 +29,8 @@ void loop() {
   int s3 = x1.substring(6,9).toInt(); // using their position on the string. 
   int s4 = x1.substring(9,12).toInt();
   int s5 = x1.substring(12,15).toInt();
-  Serial.print(s1); //Printing into the Serial the first recieved value
+  String to_send = "  " + String(b1) + "  " + String(b2) + "  ";
+  Serial.print(to_send);
   servo1.write(s1); 
   servo2.write(s2); //Sending all Data to each servo
   servo3.write(s3);
